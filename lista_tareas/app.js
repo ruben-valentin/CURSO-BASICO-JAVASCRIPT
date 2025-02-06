fb8999da996f0332b9f54e4c418d1f05a59d955c
RubenValentin <rubenvalentin.caravaca@murciaeduca.es>
2025-02-06 19:23:13 +0100

    add task app

---
+++ lista_tareas/app.js
@@ -0,0 +1,57 @@
+let boton = document.getElementById("add");
+let lista = document.querySelector("ul");
+let inputTexto = document.getElementById("texto");
+
+// function saludarConNombre(nombre) {
+//     console.log(`Hola, ${nombre}!`);
+// }
+
+function addTask() {
+    if (inputTexto.value !== "") {
+        let nuevoItem = document.createElement("li");
+        nuevoItem.textContent = inputTexto.value; // Esto es el valor que hay escrito en el input
+        nuevoItem.addEventListener("click", function () {
+            console.log(nuevoItem);
+            console.log("Borrando tarea: " + nuevoItem.textContent);
+            nuevoItem.remove();
+        });
+        lista.appendChild(nuevoItem);
+        addButtonBorrar(nuevoItem);
+    }
+};
+
+
+function addButtonBorrar(nuevoItem) {
+    let buttonBorrar = document.createElement("button");
+    buttonBorrar.textContent = "Borrar";
+    buttonBorrar.setAttribute("class", " mx-3 btn btn-primary");
+    console.log(buttonBorrar);
+    nuevoItem.appendChild(buttonBorrar);
+};
+
+
+boton.addEventListener("click", function () {
+    addTask();
+});
+
+inputTexto.addEventListener('keypress', function (e) {
+    console.log(e.key);
+    if (e.key === 'Enter') {
+        addTask();
+    }
+});
+
+
+
+
+// let lista = document.querySelector("ul");
+// let nuevoItem = document.createElement("li");
+// nuevoItem.textContent = "Elemento nuevo a borrar";
+// nuevoItem.setAttribute("data-id", "2");
+// nuevoItem.addEventListener("click", function () {
+//     alert("Botón clickeado", nuevoItem.getAttribute("data-id"));
+//     nuevoItem.remove();
+// });
+// lista.appendChild(nuevoItem);
+
+
